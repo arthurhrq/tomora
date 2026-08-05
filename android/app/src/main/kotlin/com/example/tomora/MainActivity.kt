71e5e822f86c58a148a99fc6e0dc6016397780e8
Arthur <arthurhenrisan@icloud.com>
2026-08-05 13:29:38 -0300

    fisrt-commit

---
+++ android/app/src/main/kotlin/com/example/tomora/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tomora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const cordefundo = Color(0xFF1C2F3F);
+  static const roxo = Color(0xFFA79CD6);
+  static const placeholder = Color(0xFF2C3C4A);
+  static const contorno = Color(0xFF62778A);
+  static const branco = Color(0xFFFFFFFF);
+}
